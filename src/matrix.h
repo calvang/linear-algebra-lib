9923e704ae89161c6d34d1bc8d2428dc2098eb98
calvang <calvin.huang5@gmail.com>
2020-08-14 14:32:21 -0400

    Update assignment operator

--- src/matrix.h
+++ src/matrix.h
@@ -11,11 +11,11 @@ class Matrix {
 	public:
 		Matrix() : height(0), width(0) {}
 		// construct n x n Matrix 
-		Matrix(int n) : height(n), width(n) { 
+		Matrix(size_t n) : height(n), width(n) { 
 			matrix=vector<vector<double>>(n, vector<double>(n));
 		}
 		// construct rows x cols Matrix
-		Matrix(int rows, int cols) : height(rows), width(cols) {
+		Matrix(size_t rows, size_t cols) : height(rows), width(cols) {
 			matrix=vector<vector<double>>(rows, vector<double>(cols));
 		}
 		// construct from a vector
@@ -24,26 +24,12 @@ class Matrix {
 		// TODO: implement this with std::copy after iterator is implemented
 		Matrix(Matrix& m) : height(m.rows()), width(m.cols()) {
 			if (&m == this) return;
-			// if (height!=m.rows() && width!=m.cols())
-			// 	matrix=vector<vector<double>>(height, vector<double>(width));
-			// for (int row=0;row<height;++row) {
-			// 	for (int col=0;col<width;++col) {
-			// 		matrix[row][col]=m[row][col];
-			// 	}
-			// }
 			matrix = *(m.getData());
 		}
 		// assignment operator
 		Matrix& operator=(Matrix& m) {
 			std::cout << "GETTING DATA\n";
 			if (&m == this) return *this;
-			// if (height!=m.rows() && width!=m.cols())
-			// 	matrix=vector<vector<double>>(height, vector<double>(width));
-			// for (int row=0;row<height;++row) {
-			// 	for (int col=0;col<width;++col) {
-			// 		matrix[row][col]=m[row][col];
-			// 	}
-			// }
 			std::cout << "GETTING DATA\n";
 			matrix = *(m.getData());
 			std::cout << "GETTING DATA\n";
@@ -53,29 +39,29 @@ class Matrix {
 		bool operator==(Matrix& m) {
 			if (height!=m.rows() && width!=m.cols())
 				return false;
-			for (int row=0;row<height;++row) {
-				for (int col=0;col<width;++col) {
+			for (size_t row=0;row<height;++row) {
+				for (size_t col=0;col<width;++col) {
 					if (matrix[row][col]!=m[row][col]) return false;
 				}
 			}
 			return true;
 		}
 		// index operator
-		vector<double>& operator[](int index) {
+		vector<double>& operator[](size_t index) {
 			return matrix[index];
 		}
 
 		
-		int size() { return width * height; }
-		int rows() { return height; }
-		int cols() { return width; }
+		size_t size() { return width * height; }
+		size_t rows() { return height; }
+		size_t cols() { return width; }
 
 		// multiply scalar by Matrix
 		friend Matrix operator*(const double a, Matrix& b) {
-			int h=b.rows(), w=b.cols();
+			size_t h=b.rows(), w=b.cols();
 			Matrix tmp(b);
-			for (int row=0;row<h;++row) {
-				for (int col=0;col<w;++col) {
+			for (size_t row=0;row<h;++row) {
+				for (size_t col=0;col<w;++col) {
 					tmp[row][col] = a*b[row][col];
 				}
 			}
@@ -83,10 +69,10 @@ class Matrix {
 		}
 		// multiply Matrix by scalar
 		friend Matrix operator*(Matrix& a, const double b) {
-			int h=a.rows(), w=a.cols();
+			size_t h=a.rows(), w=a.cols();
 			Matrix tmp(a);
-			for (int row=0;row<h;++row) {
-				for (int col=0;col<w;++col) {
+			for (size_t row=0;row<h;++row) {
+				for (size_t col=0;col<w;++col) {
 					tmp[row][col] = b*a[row][col];
 				}
 			}
@@ -95,14 +81,14 @@ class Matrix {
 		
 		// compute dot product
 		friend Matrix operator*(Matrix& a, Matrix& b) {
-			int ah=a.rows(), aw=a.cols(), bh=b.rows(), bw=b.cols();
+			size_t ah=a.rows(), aw=a.cols(), bh=b.rows(), bw=b.cols();
 			// TODO implement check to see if matrices are valid
-			int tmph=ah, tmpw=bh;
+			size_t tmph=ah, tmpw=bh;
 			Matrix tmp(tmph,tmpw);
-			for (int row=0;row<tmph;++row) {
-				for (int col=0;col<tmpw;++col) {
-					int product=0;
-					for (int i=0;i<aw;++i) product+=a[row][i]*b[col][i];
+			for (size_t row=0;row<tmph;++row) {
+				for (size_t col=0;col<tmpw;++col) {
+					size_t product=0;
+					for (size_t i=0;i<aw;++i) product+=a[row][i]*b[col][i];
 					tmp[row][col]=product;
 				}
 			}
@@ -113,21 +99,33 @@ class Matrix {
 		vector<vector<double>>* getData() {
 			return &matrix;
 		}
-
+		
+		// prsize_t matrix
 		void print() {
-			for (int row=0;row<height;++row) {
-				std::cout << "|";
-				for (int col=0;col<width;++col) {
+			for (size_t row=0;row<height;++row) {
+				std::cout << "[";
+				for (size_t col=0;col<width;++col) {
 					std::cout << " " << matrix[row][col];
 				}
-				std::cout << " |\n";
+				std::cout << " ]\n";
+			}
+		}
+		// prsize_t with header message
+		void print(std::string message) {
+			std::cout << message << "\n";
+			for (size_t row=0;row<height;++row) {
+				std::cout << "[";
+				for (size_t col=0;col<width;++col) {
+					std::cout << " " << matrix[row][col];
+				}
+				std::cout << " ]\n";
 			}
 		}
 
 	private:
 		vector<vector<double>> matrix;
-		int width;
-		int height;
+		size_t width;
+		size_t height;
 };
 
 // Matrix& cross(Matrix& a, Matrix& b) {
--- tests/testMatrix.cpp
+++ tests/testMatrix.cpp
@@ -7,7 +7,10 @@ void test_assignment();
 void test_dot_prod();
 
 int main(int argc, char** argv) {
+    std::cout << "Running test suite...\n";
     test_print();
+    test_copy_constructor();
+    test_assignment();
     return 0;
 }
 
@@ -18,37 +21,36 @@ void printVector(vector<double> vec) {
 }
 
 void test_print() {
+    std::cout << "\n------test_print------\n";
     Matrix mat1(4, 4);
-    mat1.print();
-    vector<vector<double>> matVec;
-    for (size_t i = 0; i < 4; ++i) {
-        std:: cout <<  "new vec \n";
-        vector<double> tmp;
-        for (size_t j = 0; j < 4; ++j) {
-            std:: cout <<  " " << j << "\n";
-            tmp.push_back(j);
-        }
-        matVec.push_back(tmp);
-    }
-    Matrix mat2(matVec);
-    mat2.print();
-    Matrix mat3(mat2);
-    mat3.print();
+    mat1.print("mat1:");
 }
 
 void test_copy_constructor() {
+    std::cout << "\n------test_copy_constructor------\n";
     vector<vector<double>> matVec;
     for (size_t i = 0; i < 4; ++i) {
-        std:: cout <<  "new vec \n";
         vector<double> tmp;
         for (size_t j = 0; j < 4; ++j) {
-            std:: cout <<  " " << j << "\n";
             tmp.push_back(j);
         }
         matVec.push_back(tmp);
     }
     Matrix mat2(matVec);
-    mat2.print();
+    mat2.print("mat1:");
     Matrix mat3(mat2);
-    mat3.print();
+    mat3.print("mat2:");
+}
+
+void test_assignment() {
+    std::cout << "\n------test_assignment------\n";
+    vector<vector<double>> matVec;
+    for (size_t i = 0; i < 4; ++i) {
+        vector<double> tmp = { 2, 3, 5, 4, 7, 8 };
+        matVec.push_back(tmp);
+    }
+    Matrix mat1(matVec);
+    mat1.print("mat1:");
+    Matrix mat2 = mat1;
+    mat2.print("mat2");
 }
